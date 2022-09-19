6060e36c74c3228053af7fa09ac4c78cff0ccbe6
Zachary Freilino <zlf1001@sru.edu>
2022-09-19 19:40:18 -0400

    Adds emailSentConfirm to twoFactorLogin

--- WebBasedEvaluations/src/main/java/edu/sru/group3/WebBasedEvaluations/controller/TwoFactorLoginController.java
+++ WebBasedEvaluations/src/main/java/edu/sru/group3/WebBasedEvaluations/controller/TwoFactorLoginController.java
@@ -2,30 +2,49 @@ package edu.sru.group3.WebBasedEvaluations.controller;
 
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import edu.sru.group3.WebBasedEvaluations.domain.PasswordResetToken;
 import edu.sru.group3.WebBasedEvaluations.domain.ResetPassword;
 import edu.sru.group3.WebBasedEvaluations.domain.User;
+import edu.sru.group3.WebBasedEvaluations.repository.PasswordTokenRepository;
+import edu.sru.group3.WebBasedEvaluations.repository.UserRepository;
+import edu.sru.group3.WebBasedEvaluations.service.AdminMethodsService;
+import edu.sru.group3.WebBasedEvaluations.service.EmailSenderService;
 
 public class TwoFactorLoginController {
-	public String loginRequest(@ModelAttribute ResetPassword reset, Model model) {
+	
+	@Autowired
+	private EmailSenderService service;
+	@Autowired
+	private PasswordTokenRepository passwordTokenRepository;
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private AdminMethodsService adminMethodsService;
+
+	private static final int PASSWORDSIZE = 5;
+	
+	public String loginRequest(@ModelAttribute ResetPassword login, Model model) {
 	String token = UUID.randomUUID().toString();
 	String path = "http://localhost:8080";
 	String url = path + "/loginRequest?token=" + token;
 	String ansr;
 	String mess;
-	System.out.println(reset.getEmail());
-	System.out.println(reset.getPassword());
+	System.out.println(login.getEmail());
+	System.out.println(login.getPassword());
 
 	// LocalDate localDate = LocalDate.now();
-	model.addAttribute("reset", reset);
+	model.addAttribute("reset", login);
 	try {
 		ansr = "pass";
 		mess = "Email sent";
 
-		User user = userRepository.findByEmail(reset.getEmail());
+		User user = userRepository.findByEmail(login.getEmail());
 
 		PasswordResetToken passwordResetToken = new PasswordResetToken(token, user);
 
@@ -33,7 +52,7 @@ public class TwoFactorLoginController {
 
 		passwordTokenRepository.save(passwordResetToken);
 
-		service.sendSimpleEmail(reset.getEmail(),
+		service.sendSimpleEmail(login.getEmail(),
 				"The reset token associated with this url will expire in 10 minutes. \n" + url, "Password Reset");
 
 		model.addAttribute("ansr", ansr);
@@ -52,6 +71,17 @@ public class TwoFactorLoginController {
 	model.addAttribute("ansr", ansr);
 
 	return "login";
-}
+	}
+	
+	@GetMapping("/email_sent")
+	public void emailSentConfirm(Model model) {
+		String ansr = "Email has been sent";
+		String succ = "test";
+
+		model.addAttribute("test", succ);
+		model.addAttribute("mess", ansr);
+
+		model.addAttribute("ansr", ansr);
+	}
 
 }
