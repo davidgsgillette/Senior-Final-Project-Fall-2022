1846c269f257d64bae00023a8e1e993941f13ff7
Duncan Lawrence <dal1017@sru.edu>
2022-09-26 13:29:21 -0400

    Tried to implement Suites and got close. still dependency issues

--- WebBasedEvaluations/src/test/java/edu/sru/group3/WebBasedEvaluations/WebBasedEvaluationsApplicationTests.java
+++ WebBasedEvaluations/src/test/java/edu/sru/group3/WebBasedEvaluations/WebBasedEvaluationsApplicationTests.java
@@ -1,21 +1,15 @@
 package edu.sru.group3.WebBasedEvaluations;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.suite.api.IncludeClassNamePatterns;
+import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.platform.suite.api.SuiteDisplayName;
 
-@ExtendWith(Suite.class)
-@Suite.SuiteClasses({
-	EvaluationTests.class})
+
+@Suite
+@SelectPackages("edu.sru.group3.WebBasedEvaluations")
+@SuiteDisplayName("A suite of tests so we don't have to do each")
 class WebBasedEvaluationsApplicationTests {
+	
 
 }
